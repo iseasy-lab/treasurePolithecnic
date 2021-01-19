1613dfdac0cb647056d7d65d8e45eea0108e6fb4
Jonathan Caiza <jpaul.snp@gmail.com>
2021-01-19 11:01:06 -0500

    Buil del juego
    Build del juego

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   firebaseConfig: {
     apiKey: "AIzaSyAwynWQEFlZdI4BW192aYOpGNiKrPDzRwU",
     authDomain: "epn-game.firebaseapp.com",
